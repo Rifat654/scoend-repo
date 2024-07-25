7061a7f24a987c54c5230286512d32e928d7e834
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2024-07-25 21:35:22 +0600

    modified menu bar

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function name(params) {
-    
+let ul = document.getElementById("ul");
+ul.style.maxHeight = "0px";
+
+function toggleMenu() {
+  
+    if (ul.style.maxHeight == "0px") 
+    {
+    ul.style.maxHeight = "300px";
+  } 
+
+  else{
+    ul.style.maxHeight = "0px";
+ }
+
 }
